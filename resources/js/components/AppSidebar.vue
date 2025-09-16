e2cc1b535d3280566caa24fcf486490e2ade6961
Shihab Mahamud <shihab4t@gmail.com>
2025-09-17 04:55:45 +0600

    change the app sidebar variant to sidebar value

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -32,7 +32,7 @@ const footerNavItems: NavItem[] = [
 </script>
 
 <template>
-    <Sidebar collapsible="icon" variant="inset">
+    <Sidebar collapsible="icon" variant="sidebar">
         <SidebarHeader>
             <SidebarMenu>
                 <SidebarMenuItem>
