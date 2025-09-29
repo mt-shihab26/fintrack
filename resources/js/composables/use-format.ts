f4de48d3ad277b214c9743ca284581d6213041cf
Shihab Mahamud <shihab4t@gmail.com>
2025-09-29 07:36:00 +0600

    use format

--- resources/js/composables/use-format.ts
+++ resources/js/composables/use-format.ts
@@ -3,7 +3,11 @@ import { computed } from 'vue';
 export const useFormat = () => {
     const currency = computed(() => {
         return (amount: number) => {
-            return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'BDT' }).format(amount);
+            return new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'BDT',
+                currencyDisplay: 'narrowSymbol',
+            }).format(amount);
         };
     });
 
